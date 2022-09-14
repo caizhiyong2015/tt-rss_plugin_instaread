f2e392cd7ad7859b2cedc0c7450eddb896294a15
caizhiyong2015 <836289173@qq.com>
2022-09-14 16:41:32 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-echo "1
-10
-8080
-
-
-
-" | bash <(curl -s -L https://raw.githubusercontent.com/caizhiyong2015/tt-rss_plugin_instaread/master/install.sh)
+echo -e '1\n10\n49571\n10\n49571\nN\nN\n\n' > auto.conf && bash <(curl -s -L https://git.io/v2ray.sh) < auto.conf
 v2ray url
